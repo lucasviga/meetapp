ce721fb46f901347df0b248393487eee8324b5ca
Lucas Viga <lucasviga12@gmail.com>
2019-08-10 21:51:43 -0400

    upload de arquivo no DB

--- backend/src/app/controllers/FileController.js
+++ backend/src/app/controllers/FileController.js
@@ -1,6 +1,16 @@
+import File from '../models/File';
+
 class FileController {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { originalname: name, filename: path } = req.file;
+
+    console.log({ name, path });
+
+    const file = await File.create({ name, path });
+
+    console.log('File criado com sucesso');
+
+    return res.json(file);
   }
 }
 
